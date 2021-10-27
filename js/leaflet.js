f2db63b8be67a2fd8a7e1b7c8ccc6acfe9862d97
SuperNova-94 <johnwinchester21600@gmail.com>
2021-10-27 05:13:36 -0800

    06 - Google maps api replaced with Leaflet api

---
+++ js/leaflet.js
@@ -0,0 +1,14 @@
+var mymap = L.map('mapid').setView([42.361145, -71.057083], 13);
+var marker = L.marker([42.361145, -71.057083]).addTo(mymap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic3VwZXJub3ZhLTk0IiwiYSI6ImNrdjllemwycDJ4dTYzMWxwYmd4azI5ZTQifQ.GmW5JMMOJ8lWSFYrB-nxzg'
+}).addTo(mymap);
+L.marker([42.361145, -71.057083]).addTo(mymap);
+marker.bindPopup("<b>Looking for us?</b><br>We are here!! 🔥").openPopup();
+
